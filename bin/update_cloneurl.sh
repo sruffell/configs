e7a4d1b6195e9f79c7dc2eddb1dd593000446529
Shaun Ruffell <sruffell@digium.com>
2018-03-07 12:41:27 -0600

    Spring cleaning.
    This project has grown stale but I find myself wanting to move my
    configuration between many new systems and wnat a place to store.
    
    Most of the stuff that I'm eliminating here were useful while I was at Digium
    but really didn't end up getting used much.

--- bin/update_cloneurl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-USERNAME=`whoami`
-HOSTNAME=`hostname -f`
-REPO_NAME=${1}
-
-for dir in `ls /home/${USERNAME}/public_git/`; do
-	echo "read-only: git://${HOSTNAME}/~${USERNAME}/${dir}" > /home/${USERNAME}/public_git/${dir}/cloneurl
-	echo "read-write: ${USERNAME}@${HOSTNAME}:public_git/${dir}" >> /home/${USERNAME}/public_git/${dir}/cloneurl
-done
